6153583168afbea37e6ae0458a6a4ad68b97caa1
Isao Hara <haraisao@gmail.com>
2019-09-06 16:48:30 +0900

    Add GoogleTextToSpeechRTC

---
+++ openhrivoice/GoogleTextToSpeechRTC/GoogleTextSpeechRTC.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Google Text to Speech component
+
+Copyright (C) 2019
+    Isao Hara
+    National Institute of Advanced Industrial Science and Technology (AIST),
+    Japan
+    All rights reserved.
+
+Licensed under the Eclipse Public License -v 1.0 (EPL)
+http://www.opensource.org/licenses/eclipse-1.0.txt
+'''
+
+import sys, os, socket, subprocess, signal, threading, platform
+import time, struct, traceback, getopt, wave, tempfile
+import optparse
+
+import json, base64
+import urllib
+import urllib.request, urllib.error, urllib.parse
+
+from pydub import AudioSegment
+from pydub.silence import *
+
+from xml.dom.minidom import Document
+
+import OpenRTM_aist
+import RTC
+from __init__ import __version__
+import utils
+
+
+__doc__ = 'Google Text-to-Speech component.'
+
+#
+#  
+#
+class GoogleTextToSpeechWrap(object):
+    
+    #
+    #  Constructor
+    #
+    def __init__(self, rtc, language='ja-JP'):
+        self._endpoint = "https://texttospeech.googleapis.com/v1/text:synthesize"
+        self._lang = "ja-JP"
+        self._speekingRate=1.0
+        self._apikey = ""
+
+        prop = rtc._properties
+        if prop.getProperty("google.speech.apikey") :
+            self._apikey=prop.getProperty("google.speech.apikey")
+
+        if prop.getProperty("google.tts.lang") :
+            self._lang=prop.getProperty("google.tts.lang")
+
+        if prop.getProperty("google.tts.logdir") :
+            self._logdir=prop.getProperty("google.tts.logdir")
+
+    #
+    #  Set ApiKey
+    #
+    def set_apikey(self, key):
+        self._apikey = key
+
+    #
+    #
+    def text2speech(self, text):
+        url = self._endpoint+"?key="+self._apikey
+        headers = {  'Content-Type' : 'application/json; charset=utf-8' }
+
+        data = { "input": { "text" : text }, 
+                 "voice" : {  'languageCode' : self._lang  # en-US, ja-JP, fr-FR
+                             , 'name':'ja-JP-Standard-A'
+                             , 'ssmlGender':'NEUTRAL' # MALE, FEMALE, NEUTRAL
+                         },
+                 'audioConfig':{ 
+                   'audioEncoding':'LINEAR16'  # LINEAR16, MP3, OGG_OPUS
+                   , 'speakingRate' : self._speekingRate # [0.25: 4.0]
+                   #, 'pitch' : 0.0  [ -20.0: 20.0]
+                   #, volumeGainDb : 0.0 
+                   , 'sampleRateHertz' : 16000  # default is 22050
+                   #,  'effectsProfileId' : "wearable-class-device" # handset-class-device, headphone-class-device,	small-bluetooth-speaker-class-device, medium-bluetooth-speaker-class-device,large-home-entertainment-class-device, large-automotive-class-device, telephony-class-applicatio
+                 }
+          }
+
+        request = urllib.request.Request(url, data=json.dumps(data).encode(), headers=headers)
+
+        try:
+            result = urllib.request.urlopen(request)
+        except urllib.error.HTTPError as e:
+            print ('Error code:', e.code)
+            print ('Reason:', e.reason)
+            return ""
+        except urllib.error.URLError as e:
+            print ('URLError reason:', e.reason)
+            return ""
+        else:
+            response = result.read()
+            return response
+
+#
+#  GoogleSpeechRecogRTC 
+#
+GoogleTextToSpeechRTC_spec = ["implementation_id", "GoogleTextToSpeechRTC",
+                  "type_name",         "GoogleTextToSpeechRTC",
+                  "description",       __doc__,
+                  "version",           __version__,
+                  "vendor",            "AIST",
+                  "category",          "communication",
+                  "activity_type",     "DataFlowComponent",
+                  "max_instance",      "1",
+                  "language",          "Python",
+                  "lang_type",         "script",
+
+                  "conf.default.lang", "ja-JP",
+                  "conf.__widget__.lang", "text",
+                  "conf.__type__.lang", "string",
+
+                  ""]
+#
+#  DataListener class
+#
+class DataListener(OpenRTM_aist.ConnectorDataListenerT):
+    #
+    #  Constructor
+    #
+    def __init__(self, name, obj, dtype):
+        self._name = name
+        self._obj = obj
+        self._dtype = dtype
+    
+    #
+    #  
+    #
+    def __call__(self, info, cdrdata):
+        data = OpenRTM_aist.ConnectorDataListenerT.__call__(self, info, cdrdata, self._dtype(RTC.Time(0,0), ""))
+        print(data, self._name)
+        self._obj.onData(self._name, data)
+
+#
+#  GoogleTextToSpeechRTC Class
+#
+class GoogleTextToSpeechRTC(OpenRTM_aist.DataFlowComponentBase):
+    #
+    #  Constructor
+    #
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+        self._tts = None
+        self._copyrights=[]
+        self._lang = [ "ja-JP" ]
+
+
+    #
+    #  OnInitialize
+    #
+    def onInitialize(self):
+        OpenRTM_aist.DataFlowComponentBase.onInitialize(self)
+        self._logger = OpenRTM_aist.Manager.instance().getLogbuf(self._properties.getProperty("instance_name"))
+        self._logger.RTC_INFO("GoogleTextToSpeechRTC version " + __version__)
+        self._logger.RTC_INFO("Copyright (C) 2019 Isao Hara")
+        #
+        #
+        self.bindParameter("lang", self._lang, "ja-JP")
+
+        #
+        # create inport for audio stream
+        self._indata = RTC.TimedString(RTC.Time(0,0), "")
+        self._inport = OpenRTM_aist.InPort("data", self._indata)
+        self._inport.appendProperty('description', 'String to be convet to audio data.')
+        self._inport.addConnectorDataListener(OpenRTM_aist.ConnectorDataListenerType.ON_BUFFER_WRITE,
+                                              DataListener("data", self, RTC.TimedString))
+        self.registerInPort(self._inport._name, self._inport)
+
+        #
+        # create outport for result
+        self._outdata = RTC.TimedOctetSeq(RTC.Time(0,0), None)
+        self._outport = OpenRTM_aist.OutPort("result", self._outdata)
+        self._outport.appendProperty('description', 'Result audio data of TTS.')
+        self.registerOutPort(self._outport._name, self._outport)
+
+        self._logger.RTC_INFO("This component depends on following softwares and data:")
+        self._logger.RTC_INFO('')
+        for c in self._copyrights:
+            for l in c.strip('\n').split('\n'):
+                self._logger.RTC_INFO('  '+l)
+            self._logger.RTC_INFO('')
+
+        return RTC.RTC_OK
+
+    #
+    #  OnFinalize
+    #
+    def onFinalize(self):
+        if self._tts:
+            self._tts.terminate()
+        OpenRTM_aist.DataFlowComponentBase.onFinalize(self)
+        return RTC.RTC_OK
+
+    #
+    #  OnActivate
+    #
+    def onActivated(self, ec_id):
+        self._tts = GoogleTextToSpeechWrap(self, self._lang[0])
+
+        OpenRTM_aist.DataFlowComponentBase.onActivated(self, ec_id)
+
+        if self._tts._apikey:
+            return RTC.RTC_OK
+        else:
+            print("=== No API KEY ===")
+            return RTC.RTC_ERROR
+
+    #
+    #  OnDeactivate
+    #
+    def onDeactivate(self, ec_id):
+        self._recog.terminate()
+        OpenRTM_aist.DataFlowComponentBase.onDeactivate(self, ec_id)
+        return RTC.RTC_OK
+
+    #
+    #  OnData (Callback from DataListener)
+    #
+    def onData(self, name, data):
+        if self._tts:
+            if name == "data":
+                txt=data.data.encode('raw-unicode-escape').decode()
+                response = self._tts.text2speech(txt)
+                
+                if response :
+                    result=json.loads(response.decode())
+                    audio = base64.b64decode(result['audioContent'].encode())
+
+                    if len(audio) > 44:
+                        self._outdata.data = audio[44:]
+                        self._outport.write(self._outdata)
+
+
+    #
+    #  OnExecute (Do nothing)
+    #
+    def onExecute(self, ec_id):
+        OpenRTM_aist.DataFlowComponentBase.onExecute(self, ec_id)
+        return RTC.RTC_OK
+
+
+#
+#  Manager Class
+#
+class GoogleTextToSpeechManager:
+    #
+    #  Constructor
+    #
+    def __init__(self):
+
+        parser = utils.MyParser(version=__version__, description=__doc__)
+        utils.addmanageropts(parser)
+
+        try:
+            opts, args = parser.parse_args()
+        except optparse.OptionError as e:
+            print( 'OptionError:', e, file=sys.stderr)
+            sys.exit(1)
+
+        self._comp = None
+        self._manager = OpenRTM_aist.Manager.init(utils.genmanagerargs(opts))
+        self._manager.setModuleInitProc(self.moduleInit)
+        self._manager.activateManager()
+
+    #
+    #  Start component
+    #
+    def start(self):
+        self._manager.runManager(False)
+
+    def shutdown(self):
+        self._manager.shutdown()
+
+    #
+    #  Initialize rtc
+    #
+    def moduleInit(self, manager):
+        profile = OpenRTM_aist.Properties(defaults_str = GoogleTextToSpeechRTC_spec)
+        manager.registerFactory(profile, GoogleTextToSpeechRTC, OpenRTM_aist.Delete)
+
+        self._comp = manager.createComponent("GoogleTextToSpeechRTC?exec_cxt.periodic.rate=1")
+
+#
+#
+g_manager = None
+
+#
+# 
+def sig_handler(num, frame):
+    global g_manager
+    if g_manager:
+        g_manager.shutdown()
+#
+#
+#
+def main():
+    global g_manager
+    signal.signal(signal.SIGINT, sig_handler)
+    g_manager = GoogleTextToSpeechManager()
+    g_manager.start()
+
+
+#
+#  Main
+#
+if __name__=='__main__':
+    main()
